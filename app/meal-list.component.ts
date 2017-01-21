6dbb409114d89613544bf40c29dbec8e79969989
quinnschwartz <quinnschwartz@gmail.com>
2017-01-20 17:06:55 -0800

    attempt to implement a pipe to filter meals displayed in the list based on calorie value, so far unsuccessful but trying to isolate bugs.

--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -4,6 +4,12 @@ import { Meal } from './meal.model';
 @Component({
   selector: 'meal-list',
   template: `
+  <select (change)="onChange($event.target.value)">
+    <option value="allMeals">All Meals</option>
+    <option value="healthyMeals" selected="selected">Healthy Meals</option>
+    <option value="unhealthyMeals">Unhealthy Meals</option>
+  </select>
+
   <ul>
    <li *ngFor="let currentMeal of childMealList">{{currentMeal.type}}: {{currentMeal.dish}} ({{currentMeal.calories}} calories)<button (click)="editButtonHasBeenClicked(currentMeal)">Edit!</button></li>
  </ul>
@@ -14,6 +20,13 @@ import { Meal } from './meal.model';
 export class MealListComponent {
   @Input() childMealList: Meal[];
   @Output() clickSender = new EventEmitter();
+
+  filterByCalories: string = "unhealthyMeals";
+
+  onChange(optionFromMenu) {
+    this.filterByCalories = optionFromMenu;
+  }
+
 
   editButtonHasBeenClicked(mealToEdit: Meal) {
     this.clickSender.emit(mealToEdit);
